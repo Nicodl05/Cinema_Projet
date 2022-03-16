5fdc89f4a968888050688cf711dbdb5fb7e41a6e
Nicodl05 <78737983+Nicodl05@users.noreply.github.com>
2022-03-16 18:57:34 +0100

    Add_API
    Ajout de l'API avec les dépendencies
    Ajout de la fonction test_api pour bon fonctionnement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.cinema_projet {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires themoviedbapi;
 
     opens com.example.cinema_projet to javafx.fxml;
     exports com.example.cinema_projet;
