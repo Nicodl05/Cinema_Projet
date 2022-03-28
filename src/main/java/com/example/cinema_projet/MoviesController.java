bf14767384412b2d07aa3401e2a1674735ee9422
AdrienLeboeuf <adrien.leboeuf01@gmail.com>
2022-03-28 11:53:45 +0200

    début movie jcrois ^^

--- src/main/java/com/example/cinema_projet/MoviesController.java
+++ src/main/java/com/example/cinema_projet/MoviesController.java
@@ -1,9 +1,13 @@
 package com.example.cinema_projet;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -24,20 +28,32 @@ public class MoviesController implements Initializable {
         //GetAllImageUrl();
 
     }
-    public void CreateAllButtonToDisplay(String ImageUrl){
+    public void CreateAllButtonToDisplay(){
+
+        int a =100;
+        int b = 100;
+        for(int i=0;i<ButtonList.size();i++){
+
+            a +=100;
+            b +=100;
+            if(i%3==0){
+                a =100;
+            }
+            ButtonList.get(i).setLayoutX(a);
+            ButtonList.get(i).setLayoutY(b);
+
+        }
+
+    }
+    public void CreateAButton(String ImageUrl){
 
         ImageView view = new ImageView(ImageUrl);
         Button button1 = new Button("",view);
 
         ButtonList.add(button1);
-
-        ButtonList.get(1).setOnAction();
-
-
-
     }
-    
-    public void CreateAButton(){
+
+    public void FindAMovie(String textfield){
 
 
     }
