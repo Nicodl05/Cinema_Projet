3f5531016791c9dee00e5260260bccb5b04d8bbe
AdrienLeboeuf <adrien.leboeuf01@gmail.com>
2022-04-02 15:02:24 +0200

    update

--- src/main/java/com/example/cinema_projet/LoginController.java
+++ src/main/java/com/example/cinema_projet/LoginController.java
@@ -4,6 +4,7 @@ import controller.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,10 +16,13 @@ import javafx.stage.Stage;
 import model.User;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.*;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     @FXML
     private TextField MyMail;
@@ -27,34 +31,66 @@ public class LoginController {
     private TextField MyPassword;
 
     @FXML
-    private Label MailLabel;
+    public Label MailLabel;
 
     @FXML
-    private Label PasswordLabel;
+    public Label PasswordLabel;
 
     public static User personne= new User();
 
+    public LoginController(){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
     public void ConnOnAction() {
         LoginAccountCreate login = new LoginAccountCreate();
-        personne=login.login(MyMail.getText(),MyPassword.getText());
-        try {
-            Connected();
-        } catch (IOException e) {
-            e.printStackTrace();
+        personne = login.login(MyMail.getText(), MyPassword.getText());
+
+        if(Objects.equals(personne.getLastName(), "wrong email")) {
+            MailLabel.setText("Mauvais mail");
+
         }
+        if(Objects.equals(personne.getLastName(), "wrong password")) {
+            PasswordLabel.setText("Mauvais Password");
+        }
+        if(Objects.equals(personne.getLastName(), "wrong password and email")) {
+            MailLabel.setText("Mauvais mail");
+            PasswordLabel.setText("Mauvais Password");
+
+        }
+
+
     }
 
     public void Connected() throws IOException {
 
 
-            Parent fxmlLoader = FXMLLoader.load(getClass().getResource("Movies.fxml"));
-            Scene scene = new Scene(fxmlLoader,800,600);
-            Stage stage = new Stage();
-            //stage.setTitle("Hello!");
-            stage.setScene(scene);
-            stage.show();
-
-        }
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("Movies.fxml"));
+        Scene scene = new Scene(fxmlLoader, 800, 600);
+        Stage stage = new Stage();
+        //stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
 
     }
+
+    public void ForgetPassword(ActionEvent event) throws IOException {
+
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("Movies.fxml"));
+        Scene scene = new Scene(fxmlLoader, 800, 600);
+        Stage stage = new Stage();
+        //stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+}
 
